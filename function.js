9f64e3859d4c426594ec7bdb1c350d8836e4d275
Belen Villalba <125991778+nievesbelenvillalba@users.noreply.github.com>
2023-02-28 17:52:04 -0300

    Add files via upload
    Cargando proyecti final

---
+++ function.js
@@ -0,0 +1,5 @@
+document.getElementById ("boton") .onclick = function (){
+    console.log ("capturamos en evento click");
+    document.getElementById("demo").innerHTML = "Tu mensaje se envió correctamente :)"
+
+}
